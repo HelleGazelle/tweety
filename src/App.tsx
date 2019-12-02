486f9b3ae00fd719a4bb7c531f210db5b1e57339
Moritz Lugbauer <moritz.lugbauer@stud.fh-rosenheim.de>
2019-12-02 12:12:53 +0100

    React Router Implementiert

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,54 @@ import TweetGrid from './component/TweetGrid';
 import Chart from './component/Chart';
 import Login from './component/Login';
 import SignUp from './component/SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header>
-        <Typography variant="h2" className='Tweety'>Tweety</Typography>
-      </header>
-      <div className='tweetGrid'>
-        <TweetGrid/>
-      </div>
-      <Chart />
-      <div>
-        <Login/>
-      </div>
-      <div>
-        <SignUp/>
-      </div>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signUp">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signUp">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <header>
+              <Typography variant="h2" className='Tweety'>Tweety</Typography>
+            </header>
+            <div className='tweetGrid'>
+              <TweetGrid/>
+            </div>
+            <Chart />
+          </div>
+        </Route>
+      </Switch>
     </div>
+    </Router>
     
   );
 }
