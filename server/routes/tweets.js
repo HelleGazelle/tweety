a54ebfeec59e38b699a14e1290b2365b5797dcf6
Alois Niedermaier <a.niedermaier@techdivision.com>
2019-12-09 13:51:06 +0100

    adding authorization

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -2,6 +2,8 @@ const express = require('express')
 const Twitter = require('twitter');
 const router = express.Router();
 const config = require('../config.js');
+var jwt = require('express-jwt');
+require('dotenv').config();
 
 const client = new Twitter(config);
 const userTimelineUrl = 'statuses/user_timeline.json?screen_name=';
@@ -9,7 +11,7 @@ const userTimelineUrl = 'statuses/user_timeline.json?screen_name=';
 const tweetsFromAccount = (accountName) => client.get(userTimelineUrl + accountName + '&count=200', {});
 
 // return tweets as json for the given screen name
-router.get('/tweets/:accountName', async (req, res) => {
+router.get('/tweets/:accountName', jwt({secret: process.env.JWT_KEY}), async (req, res) => {
     try {
         const data = await tweetsFromAccount(req.params.accountName);
         res.json(data);
