e7c948d6bf85f3d428ef24e10adf369270cffa58
HelleGazelle <32957177+HelleGazelle@users.noreply.github.com>
2019-12-27 14:17:49 +0100

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'nbH8xJDyH2nMaV9RitPGNpTYb',
-  consumer_secret: 'wHErTzTnu4r6lLF6fJJDs1RPcub69aFNcBBnsu99bZh6scEveX',
-  access_token_key: '2510603373-CKTFJ9w4kxDeJZFiOELbz7uW554Ny1QJYVTaRxQ',
-  access_token_secret: 'ZGENeXdrxGHVipXPp97TKHWzwxRGSumORlXs4sZw1Vxw0'
-}
