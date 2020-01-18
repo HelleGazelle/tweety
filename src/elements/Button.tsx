7a4ef4197ec330ba311a94883a7488afd795cec8
sWIFanseit <andrea.seitlinger@stud.fh-rosenheim.de>
2020-01-18 22:49:34 +0100

    styled Button

---
+++ src/elements/Button.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const Button = styled.button`
+background-color: #00acee;
+padding: 0.5 em;
+margin: 0.5 em;
+border-radius: 4px;
+border-color: #bebebe;
+border-style: hidden;
+font-size: 1em;
+text-align: center;
+color: #D3D3D3;
+:hover {background-color: #0097d8;}
+`;
+
+export default Button;
+
+
