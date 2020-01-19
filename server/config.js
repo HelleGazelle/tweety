fa52e91b0ed087163bc72251ec9a9815df56d304
HelleGazelle <alois.niedermaier@googlemail.com>
2020-01-19 21:26:25 +0100

    deleting config file

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'Hgm5T5ZVPn4lCkST9e8fXzdpZ',
-  consumer_secret: 'FY9dBbJLOZoGCGuOofqrg3cPRpN0zfe2tLcdyK0yX3T0sRrkDc',
-  access_token_key: '2510603373-5zVHzWir6gv0VZYspvnipiOzWHev7Gw91jUoqlK',
-  access_token_secret: 'wyLH2DK2edX015CJj3yYRGpFJqtDtRmIhR2l0A5IYlQIF'
-}
